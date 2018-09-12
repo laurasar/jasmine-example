f5475998cf47ae3e657622afd03ff5436bfb1b05
Laura Saravia <lauravs0701@gmail.com>
2018-09-11 21:39:49 -0400

    included age calculating program

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -1,4 +1,5 @@
 describe("a set of utility functions", function() {
+  //fullName (Han Solo )
   describe("fullName();" , function() {
       it("joins two strings w/ a space", function() {
         expect(fullName("Han", "Solo")).toEqual("Han Solo");
@@ -7,5 +8,12 @@ describe("a set of utility functions", function() {
       it("strips whitespace from the names", function() {
         expect(fullName("   Han", "  Solo   ")).toEqual("Han Solo");
       });
+  });
+
+  // ageInYears('1990-09-01')
+  describe("ageInYears()", function(){
+    it("returns the age from a birth date", function() {
+      expect(ageInYears("1990-09-01")).toEqual(28);
+    });
   });
 });
--- src/Utility.js
+++ src/Utility.js
@@ -1,4 +1,14 @@
+//fullName function
 function fullName(firstName, lastName) {
    return `${firstName.trim()} ${lastName.trim()}`;
 
+}
+
+//ageInYears function
+function ageInYears(birthdayStr) {
+  const birthday = new Date(birthdayStr);
+  const ageInMilliseconds = Date.now() - birthday;
+  const millisecondsPerYear = 1000 * 60 * 60 * 24 *365;
+
+  return Math.floor(ageInMilliseconds / millisecondsPerYear); 
 }
