5325ee5a1a5e8c8087725aa228cf5e50934fe4ef
Laura Saravia <lauravs0701@gmail.com>
2018-09-13 21:28:49 -0400

    added identify leap year program that works

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -19,4 +19,50 @@ describe("a set of utility functions", function() {
     });
 
   });
+
+  //describe is over 21
+  describe("isOver21()",function() {
+    beforeEach(() => {
+      const today = new Date("2017-05-15");
+      spyOn(window,"getNow").and.returnValue(today);
+    });
+
+    it("returns true when the age is 21", () => {
+      expect(isOver21("1996-01-09")).toBe(true);
+    });
+
+    it("returns true when the age is over 21", () => {
+      expect(isOver21("1992-11-02")).toBe(true);
+    });
+
+    it("returns false when the age is under 21", () => {
+      expect(isOver21("2001-04-01")).toBe(false
+
+
+
+
+
+      );
+    });
+  });
+
+  //Write a function that accepts a year (as a string or number)
+  // and returns true if the given year is a leap year.
+  // Otherwise, return false.
+  //
+  // You may already know that a leap year is a year that is
+  // evenly divisible by four, however there's more to it than that.
+  //
+  //A leap year that is divisible by 100 must also be divisible by 400 to be a leap year.
+
+  describe("isLeapYear()", function () {
+    it("Returns true when is a leap year", function() {
+      expect(isLeapYear(1600)).toEqual(true);
+      expect(isLeapYear(2008)).toEqual(true);
+    });
+    it("Returns false when it is NOT a leap year", function() {
+      expect(isLeapYear(1900)).toEqual(false);
+      expect(isLeapYear(1803)).toEqual(false);
+    });
+  });
 });
--- src/Utility.js
+++ src/Utility.js
@@ -13,5 +13,33 @@ function ageInYears(birthdayStr) {
   return Math.floor(ageInMilliseconds / millisecondsPerYear);
 }
 function getNow() {
-  return Date.now(); 
+  return Date.now();
+}
+
+function isOver21(birthdayStr) {
+  // const birthday = new Date(birthdayStr);
+  // let twentyOneYet = getNow() - birthday;
+  // let differenceAge = twentyOneYet - 21;
+  // if (differenceAge >= 0 ) {
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
+  //
+  return ageInYears(birthdayStr) >= 21;
+}
+
+function isLeapYear(year) {
+  year = Number(year);
+  let fourYear = year % 4;
+  let hundredCheck= year % 100;
+  if (fourYear != 0)  {
+   return false;
+  }
+   else if (hundredCheck != 0) {
+    return true;
+    }
+
+ // return (year % 4 ==0 ) && ((year % 100 != 0) || (year % 400 ==0 )) ;
 }
